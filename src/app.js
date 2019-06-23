dc3b18a0eb408f3d94436b93427e7bfd430c3b84
Fernando <fernando.maqhin@gmail.com>
2019-06-23 15:53:04 -0300

    Basic working configuration

---
+++ src/app.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var Apollo = require('apollo-server-express')
+
+
+const app = express();
+
+const schema = Apollo.gql`
+  type Query {
+    me: User
+  }
+
+  type User {
+    username: String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    me: () => {
+      return {
+        username: 'Robin Wieruch',
+      };
+    },
+  },
+};
+
+const server = new Apollo.ApolloServer({
+  typeDefs: schema,
+  resolvers,
+});
+
+server.applyMiddleware({ app, path: '/graphql' });
+
+app.listen({ port: 8000 }, () => {
+  console.log('Apollo Server on http://localhost:8000/graphql');
+});
