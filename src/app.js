a0ccf7dc808523c01fcd89d25ae612283a24bd4f
Fernando <fernando.maqhin@gmail.com>
2019-06-23 16:14:01 -0300

    Type Definitions

--- src/app.js
+++ src/app.js
@@ -10,19 +10,39 @@ app.use(cors());
 const schema = Apollo.gql`
   type Query {
     me: User
+    user(id: ID!): User
+    users: [User!]
   }
 
   type User {
+    id: ID!
     username: String!
   }
 `;
 
+let users = {
+  1: {
+    id: '1',
+    username: 'Robin Wieruch',
+  },
+  2: {
+    id: '2',
+    username: 'Dave Davids',
+  },
+};
+
+const me = users[1];
+
 const resolvers = {
   Query: {
+    users: () => {
+      return Object.values(users);
+    },
+    user: (parent, { id }) => {
+      return users[id];
+    },
     me: () => {
-      return {
-        username: 'Robin Wieruch',
-      };
+      return me;
     },
   },
 };
